e4ac07b718e8d0a421be74b6d42f2aa750dcae24
Eli Craig <elidcraig@gmail.com>
2022-06-29 21:11:01 -0700

    commit tree 506d80c796e9b61deca5eda8c06613cf0b74dea9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      render json: user.items
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    new_item = user.items.create(item_params)
+    render json: new_item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
